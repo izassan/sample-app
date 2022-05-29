31aa3bb4c47fd0dcc61798ab4adede3c92dbc425
izassan <izassan@mail.izassan.net>
2022-05-29 17:36:25 +0900

    add echo api

---
+++ server/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type SampleAppResponse struct {
+    Msg string `json:"msg"`
+}
+
+func echoMsg(w http.ResponseWriter, r *http.Request){
+    if r.Method != http.MethodPost {
+        w.WriteHeader(http.StatusMethodNotAllowed)
+        return
+    }
+    bufbody := new(bytes.Buffer)
+    bufbody.ReadFrom(r.Body)
+    bodyString := bufbody.String()
+    resBody := SampleAppResponse{bodyString}
+    jsonData, err := json.Marshal(resBody)
+    if err != nil{
+        log.Fatal(err)
+    }
+    log.Printf("api called. return value: %s\n", jsonData)
+    w.Write(jsonData)
+    return
+}
+
+func returnUsage() string{
+    const usage string = "listen in 'http://localhost:9000'\n" +
+        "call 'http://localhost:9000/api/echo'\n" +
+        "example:\n" +
+        "\tcurl http://localhost:9000/api/echo -X POST -d 'testmsg'\n"
+    return usage
+}
+
+func echoUsage(w http.ResponseWriter, r *http.Request){
+    usage := returnUsage()
+    w.Write([]byte(usage))
+}
+
+
+func main() {
+    http.HandleFunc("/api/echo", echoMsg)
+    http.HandleFunc("/", echoUsage)
+    fmt.Printf(returnUsage())
+    if err := http.ListenAndServe(":9000", nil); err != nil {
+        log.Fatal("ListenAndServe:", err)
+    }
+}
